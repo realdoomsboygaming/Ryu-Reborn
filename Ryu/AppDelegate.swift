b548710c816434128650741807b4489068dd792f
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-04-06 12:47:26 -0500

    Update AppDelegate.swift

--- Ryu/AppDelegate.swift
+++ Ryu/AppDelegate.swift
@@ -1,23 +1,16 @@
-//
-//  AppDelegate.swift
-//  Ryu
-//
-//  Created by Francesco on 20/06/24.
-//
-
 import UIKit
 import GoogleCast
 import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    var backgroundCompletionHandler: (() -> Void)?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         setupDefaultUserPreferences()
         setupGoogleCast()
         checkForFirstLaunch()
+        // Access DownloadManager singleton to initialize it
+        _ = DownloadManager.shared
         return true
     }
     
@@ -76,36 +69,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         if url.scheme == "ryu" {
-            if let queryParams = url.queryParameters, let code = queryParams["code"] {
+             if url.host == "anilist-callback", let queryParams = url.queryParameters, let code = queryParams["code"] {
                 NotificationCenter.default.post(name: Notification.Name("AuthorizationCodeReceived"), object: nil, userInfo: ["code": code])
+                return true
             }
+            // Handle other potential ryu:// URLs if needed
         }
-        return true
+        return GCKCastContext.sharedInstance().application(app, open: url, options: options) // Allow Cast SDK to handle URLs if needed
     }
-    
+
     // MARK: UISceneSession Lifecycle
-    
+
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-    
+
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
+
+    // Pass the completion handler to the DownloadManager
     func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
-        backgroundCompletionHandler = completionHandler
+        // Store the completion handler in DownloadManager
+        DownloadManager.shared.backgroundCompletionHandler = completionHandler
     }
-    
+
     func applicationWillTerminate(_ application: UIApplication) {
         UserDefaults.standard.set(false, forKey: "isToDownload")
         deleteTemporaryDirectory()
     }
-    
+
     func deleteTemporaryDirectory() {
         let fileManager = FileManager.default
         let tmpURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
